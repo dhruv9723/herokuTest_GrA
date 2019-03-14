c00aef22ea68c1500c9a0ccf391bc3a834cb3a96
Bhatt <d_bhatt8@tss-hr420-dm25.fc.ca>
2019-03-14 16:16:15 -0400

    fir

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message: "Heroku app1" });
+  res.render('index', { title: 'Express', message: "Heroku app2" });
 });
 
 module.exports = router;
